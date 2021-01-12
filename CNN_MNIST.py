fd9306e2c4343a6fb87a92fd4adea6a16f5852d6
jiancong952 <jiancong952@foxmail.com>
2021-01-12 14:54:17 +0800

    Initial commit

---
+++ CNN_MNIST.py
@@ -0,0 +1,41 @@
+from tensorflow.examples.tutorials.mnist import input_data
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+# 载入数据
+mnist = input_data.read_data_sets(path, one_hot=True)
+
+
+def read_data(path):
+    train_x = mnist.train.images.reshape(-1, 28, 28, 1)
+    train_y = mnist.trian.labels
+    valid_x = mnist.validation.images.reshape(-1, 28, 28, 1)
+    valid_y = mnist.validation.labels
+    test_x = mnist.test.images.reshape(-1, 28, 28, 1)
+    test_y = mnist.test.labels
+    return train_x, train_y, valid_x, valid_y, test_x, test_y
+
+
+# 序列模型
+def CNN_2D(train_x, train_y, valid_x, valid_y, test_x, test_y):
+    # 创建模型
+    model = Sequential()
+    model.add(Conv2D(input_shape=(28, 28, 1), filters=16, kernel_size=(5, 5), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))  # 最大池化
+    model.add(Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))  # 最大池化
+    model.add(Flatten())  # 扁平化
+    model.add(Dense(10, activation='softmax'))
+    # 查看网络结构
+    model.summary()
+    # 编译模型
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    # 训练模型
+    model.fit(train_x, train_y, batch_size=500, nb_epoch=20, verbose=2, validation_data=(valid_x, valid_y))
+    # 评估模型
+    pre = model.evaluate(test_x, test_y, batch_size=500, verbose=2)  # 评估模型
+    print('test_loss:', pre[0], '- test_acc:', pre[1])
+
+
+train_x, train_y, valid_x, valid_y, test_x, test_y = read_data('MNIST_data')
+CNN_2D(train_x, train_y, valid_x, valid_y, test_x, test_y)
